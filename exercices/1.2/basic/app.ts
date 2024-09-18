1bb3654193e7ecc828c9231a09f09b623a89d154
Sacha <sacha.croon@student.vinci.be>
2024-09-18 09:22:20 +0200

    new: ex1.2

--- exercices/1.2/basic/app.ts
+++ exercices/1.2/basic/app.ts
@@ -1,12 +1,27 @@
-import express from "express";
+import express, { Request, NextFunction  } from "express";
 
 import usersRouter from "./routes/users";
 import pizzaRouter from "./routes/pizzas";
 
 const app = express();
+let getRequestCount: number = 0;
+
+// Middleware pour enregistrer les statistiques des requêtes GET
+const requestStatsMiddleware = (req: Request, _res: any,next: NextFunction ) => {
+    if (req.method === 'GET') {
+        getRequestCount++;
+        console.log(`GET counter: ${getRequestCount}`);
+    }
+    next();
+}
+
+
+app.use(requestStatsMiddleware);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
