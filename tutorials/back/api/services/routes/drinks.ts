5c333d856a4b9b73be4c019331fe5957e74a7a6d
Sacha <sacha.croon@student.vinci.be>
2024-10-02 17:00:00 +0200

    services done

--- tutorials/back/api/services/routes/drinks.ts
+++ tutorials/back/api/services/routes/drinks.ts
@@ -1,73 +1,32 @@
 import { Router } from "express";
-import { Drink, NewDrink } from "../types";
-
-import path from "node:path";
-import { parse, serialize } from "../utils/json";
-const jsonDbPath = path.join(__dirname, "/../data/drinks.json");
-
-const defaultDrinks: Drink[] = [
-  {
-    id: 1,
-    title: "Coca-Cola",
-    image:
-      "https://media.istockphoto.com/id/1289738725/fr/photo/bouteille-en-plastique-de-coke-avec-la-conception-et-le-chapeau-rouges-d%C3%A9tiquette.jpg?s=1024x1024&w=is&k=20&c=HBWfROrGDTIgD6fuvTlUq6SrwWqIC35-gceDSJ8TTP8=",
-    volume: 0.33,
-    price: 2.5,
-  },
-  {
-    id: 2,
-    title: "Pepsi",
-    image:
-      "https://media.istockphoto.com/id/185268840/fr/photo/bouteille-de-cola-sur-un-fond-blanc.jpg?s=1024x1024&w=is&k=20&c=xdsxwb4bLjzuQbkT_XvVLyBZyW36GD97T1PCW0MZ4vg=",
-    volume: 0.33,
-    price: 2.5,
-  },
-  {
-    id: 3,
-    title: "Eau Minérale",
-    image:
-      "https://media.istockphoto.com/id/1397515626/fr/photo/verre-deau-gazeuse-%C3%A0-boire-isol%C3%A9.jpg?s=1024x1024&w=is&k=20&c=iEjq6OL86Li4eDG5YGO59d1O3Ga1iMVc_Kj5oeIfAqk=",
-    volume: 0.5,
-    price: 1.5,
-  },
-  {
-    id: 4,
-    title: "Jus d'Orange",
-    image:
-      "https://images.unsplash.com/photo-1600271886742-f049cd451bba?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    volume: 0.25,
-    price: 4.5,
-  },
-  {
-    id: 5,
-    title: "Limonade",
-    image:
-      "https://images.unsplash.com/photo-1583064313642-a7c149480c7e?q=80&w=1430&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    volume: 0.33,
-    price: 5,
-  },
-];
+import { NewDrink } from "../types";
+import {
+  createOneDrink,
+  deleteOneDrink,
+  readAllDrinks,
+  readOneDrink,
+  updateOneDrink,
+} from "../services/drinks";
 
 const router = Router();
 
 router.get("/", (req, res) => {
-  const drinks = parse(jsonDbPath, defaultDrinks);
-  if (!req.query["budget-max"]) {
-    // Cannot call req.query.budget-max as "-" is an operator
-    return res.json(drinks);
-  }
   const budgetMax = Number(req.query["budget-max"]);
-  const filteredDrinks = drinks.filter((drink) => {
-    if(typeof drink.price === 'number'){
-      return drink.price <= budgetMax;
-    }
-    return res.sendStatus(404); // pas de prix sur la boisson
-  });
-  return res.json(filteredDrinks);
+  const drinks = readAllDrinks(budgetMax);
+  return res.json(drinks);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const drink = readOneDrink(id);
+  if (!drink) {
+    return res.sendStatus(404);
+  }
+  return res.json(drink);
 });
 
 router.post("/", (req, res) => {
-  const body: unknown = req.body as NewDrink;
+  const body: unknown = req.body;
   if (
     !body ||
     typeof body !== "object" ||
@@ -88,60 +47,22 @@ router.post("/", (req, res) => {
   }
 
   const { title, image, volume, price } = body as NewDrink;
-  const drinks = parse(jsonDbPath, defaultDrinks);
 
-
-
- // Use reduce() to find the highest id in the pizzas array
- const nextId =
- drinks.reduce((maxId, drink): number => (drink.id! > maxId ? drink.id! : maxId), 0) +
- 1; // 0 is the initial value of maxId
-
-
-  const newDrink: Drink = {
-    id: nextId,
-    title,
-    image,
-    volume,
-    price,
-  };
-
-  drinks.push(newDrink);
-  serialize(jsonDbPath,drinks);
+  const newDrink = createOneDrink({ title, image, volume, price });
   return res.json(newDrink);
-});
-
-
-router.get("/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const drinks = parse(jsonDbPath, defaultDrinks);
-  const drink = drinks.find((drink) => drink.id === id);
-  if (!drink) {
-    return res.sendStatus(404);
-  }
-  return res.json(drink);
 });
 
 router.delete("/:id", (req, res) => {
   const id = Number(req.params.id);
-  const drinks = parse(jsonDbPath, defaultDrinks);
-  const index = drinks.findIndex((drink) => drink.id === id);
-  if (index === -1) {
+  const deletedDrink = deleteOneDrink(id);
+  if (!deletedDrink) {
     return res.sendStatus(404);
   }
-  const deletedElements = drinks.splice(index, 1); // splice() returns an array of the deleted elements
-  serialize(jsonDbPath,drinks);
-  return res.json(deletedElements[0]);
+  return res.json(deletedDrink);
 });
-
 
 router.patch("/:id", (req, res) => {
   const id = Number(req.params.id);
-  const drinks = parse(jsonDbPath,defaultDrinks);
-  const drink = drinks.find((drink) => drink.id === id);
-  if (!drink) {
-    return res.sendStatus(404);
-  }
 
   const body: unknown = req.body;
 
@@ -161,21 +82,13 @@ router.patch("/:id", (req, res) => {
 
   const { title, image, volume, price }: Partial<NewDrink> = body;
 
-  if (title) {
-    drink.title = title;
-  }
-  if (image) {
-    drink.image = image;
-  }
-  if (volume) {
-    drink.volume = volume;
-  }
-  if (price) {
-    drink.price = price;
-  }
-  serialize(jsonDbPath, drinks);
-  return res.json(drink);
-});
+  const updatedDrink = updateOneDrink(id, { title, image, volume, price });
 
+  if (!updatedDrink) {
+    return res.sendStatus(404);
+  }
+
+  return res.json(updatedDrink);
+});
 
 export default router;
