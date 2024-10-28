e3a917b7dc4a18a44ba9162af091e871f9067c80
Sacha <sacha.croon@student.vinci.be>
2024-10-28 14:47:41 +0100

    new: 2.5-destructuring

---
+++ exercices/2.5/src/components/ClickCounter.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./ClickCounter.css";
+
+interface ClickCounterProps {
+    title: string;
+    on10ClicksAction?: string;
+    onMouseOver?: string;
+    }
+
+const ClickCounter = ({
+    title,
+    on10ClicksAction = "Master !",
+    onMouseOver = "Click !",
+}: ClickCounterProps) => {
+
+    //state -etats et changements
+    const [count, setCount] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
+
+    return(
+        <div className="card">
+            <h3>{title}</h3>
+            {isHovered ? <p>{onMouseOver}</p> : null}
+            <button
+             onClick={() => setCount((count) => count+1)}
+             onMouseEnter={() => setIsHovered(true)}
+             onMouseLeave={() => setIsHovered(false)}
+            >
+            count is {count}
+            </button>
+            <button>
+            {count >= 10 ? <p>{on10ClicksAction}</p>: null}
+            
+
+            </button>
+
+
+
+        </div>
+
+    )
+
+}
+
+export default ClickCounter;
