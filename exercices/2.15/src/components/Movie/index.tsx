1bd9e6e28dd5283a9c456cea28edd3ab37903414
Sacha <sacha.croon@student.vinci.be>
2024-12-01 18:46:26 +0100

    css delete film

--- exercices/2.15/src/components/Movie/index.tsx
+++ exercices/2.15/src/components/Movie/index.tsx
@@ -1,4 +1,5 @@
 import { Movie } from "../../type";
+import "./Moviecard.css";
 
 
 interface MoviesProps {
@@ -36,14 +37,14 @@ const MovieCard = ({ movie, onMovieDeleted }: MoviesProps) => {
           </p>
         )}
       </div>
-      <div>
+      <div className="svg-div">
         <svg 
           onClick={() => onMovieDeleted(movie)}
           xmlns="http://www.w3.org/2000/svg" 
           fill="none" viewBox="0 0 24 24" 
           stroke-width="1.5" 
           stroke="currentColor" 
-          className="size-6">
+          className="svg-trash">
         <path 
           stroke-linecap="round" 
           stroke-linejoin="round"
